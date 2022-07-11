9313dc52a903e06a77c870ac1acda28d79d50542
Camilo Romero <camiloaromero23@gmail.com>
2022-07-10 22:43:26 -0500

    Include collatz & series exercises

---
+++ collatz-conjecture/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn collatz(mut n: u64) -> Option<u64> {
+    if n == 0 {
+        return None;
+    }
+    let mut steps = 0;
+
+    while n != 1 {
+        steps += 1;
+        if n % 2 == 0 {
+            n /= 2;
+            continue;
+        }
+        n = n.checked_mul(3)?.checked_add(1)?;
+    }
+    Some(steps)
+}
